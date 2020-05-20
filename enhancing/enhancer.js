4eb65d8a31e6d3cfce009d1e04cd031e12f50385
Andrew Hoffmann <ashoffmann90@gmail.com>
2020-05-20 13:40:42 -0700

    setup enhancer file

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,20 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  return item.enhancement < 20
+  ? {...item, enhancement: item.enhancement + 1} 
+  : { ...item };
 }
 
 function fail(item) {
-  return { ...item };
+  return item.enhancement < 15 ? {...item, durability: item.durability - 5}
+  : item.enhancement > 14 ? { ...item, durability: item.durability - 10 }
+  : item.enhancement > 16 ? { ...item, enhancement: item.enhancement - 1}
+  : { ...item}
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: item.durability === 100 };
 }
 
 function get(item) {
